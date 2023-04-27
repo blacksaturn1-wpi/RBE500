550fdc79de14226df766b0f21ae03d8c50d8bf83
Jason P <jpatel@wpi.edu>
2023-04-27 04:29:06 -0400

    Lab9 updates and add README.txt

--- transforms_wiki/src/tf2_dynamic_br_2.py
+++ transforms_wiki/src/tf2_dynamic_br_2.py
@@ -5,20 +5,20 @@ import geometry_msgs.msg
 import math
 
 if __name__ == '__main__':
-    rospy.init_node('dynamic_tf2_broadcaster1')
+    rospy.init_node('dynamic_tf2_broadcaster2')
     br = tf2_ros.TransformBroadcaster()
     t = geometry_msgs.msg.TransformStamped()
 
-    t.header.frame_id = "base"
-    t.child_frame_id = "frame A"
+    t.header.frame_id = "frame A"
+    t.child_frame_id = "frame B"
 
-    rate = rospy.Rate(1.0)
+    rate = rospy.Rate(1)
     while not rospy.is_shutdown():
         x = rospy.Time.now().to_sec() * math.pi
 
         t.header.stamp = rospy.Time.now()
-        t.transform.translation.x = 1 * math.sin(x)
-        t.transform.translation.y = 1 * math.cos(x)
+        t.transform.translation.x = 1 * math.cos(x)
+        t.transform.translation.y = 1 * math.sin(x)
         t.transform.translation.z = 0.0
         t.transform.rotation.x = 0.0
         t.transform.rotation.y = 0.0
